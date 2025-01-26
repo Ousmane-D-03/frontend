fff81582b26b1f6ba7f9df934adf6062e7c38662
Ousmane Sarr Diop <ousmane.diop.1@etu.u-bordeaux.fr>
2025-01-27 00:33:18 +0100

    front end

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '^/images': {
+        target: 'http://localhost:8080' // Spring boot backend address
+      }
+    }
+  }
+})
+
